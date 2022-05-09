dc42a6b646f71a3b681208ee733f49fd80478a1d
“Johan <johanblas60@gmail.com>
2022-05-09 16:41:16 -0500

    adding key properties

--- app/src/main/java/com/example/dota2lastmatch/services/ApiService.kt
+++ app/src/main/java/com/example/dota2lastmatch/services/ApiService.kt
@@ -1,5 +1,6 @@
 package com.example.dota2lastmatch.services
 
+import com.example.dota2lastmatch.BuildConfig
 import com.example.dota2lastmatch.models.Match
 import com.google.gson.JsonElement
 import com.google.gson.reflect.TypeToken
@@ -8,9 +9,18 @@ import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Url
 
+const val STEAM_API_KEY = BuildConfig.STEAM_API_KEY
+
+
 interface ApiService {
+
+
     @GET
     suspend fun getIdMatches(@Url url:String): Response<List<Match>>
     @GET
     suspend fun getMatchDetails(@Url url:String): Response<JsonElement>
+    @GET("https://api.steampowered.com/IEconDOTA2_570/GetHeroes/v1/?key=$STEAM_API_KEY")
+    suspend fun getHeroesInfo(): Response<JsonElement>
+    @GET("https://api.steampowered.com/IEconDOTA2_570/GetGameItems/v0001/?key=$STEAM_API_KEY")
+    suspend fun getItemsInfo(): Response<JsonElement>
 }
